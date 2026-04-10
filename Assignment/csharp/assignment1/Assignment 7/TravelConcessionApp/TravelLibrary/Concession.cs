6735e15137584e5c48c0949daec4900ed1a32646
Suriya Sathvika <suriyasa@infinite.com>
2026-04-10 21:02:32 +0530

    Assignment 7 Completed

---
+++ Assignment/csharp/assignment1/Assignment 7/TravelConcessionApp/TravelLibrary/Concession.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TravelLibrary
+{
+    public class Concession
+    {
+        public string CalculateConcession(string name, int age, double totalFare)
+        {
+            if (age <= 5)
+            {
+                return "Little Champs - Free Ticket";
+            }
+            else if (age > 60)
+            {
+                double discountedFare = totalFare - (totalFare * 0.30);
+                return "Senior Citizen - Fare: " + discountedFare;
+            }
+            else
+            {
+                return "Ticket Booked - Fare: " + totalFare;
+            }
+        }
+    }
+}
