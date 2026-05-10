2825d715b970ffacafebe9dfa2b9bd75356b8b1b
Suriya Sathvika <suriyasa@infinite.com>
2026-05-10 14:59:54 +0530

    Assignment 1(WebTechnologies) Completed

---
+++ Web Technologies/Assignment/Assignment 1 (WebTechnologies)/Assignment 1 (WebTechnologies)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1__WebTechnologies_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
