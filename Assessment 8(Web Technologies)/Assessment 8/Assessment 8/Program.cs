0c01a2630494fc113ee5184c22bd6bf31d02163a
Suriya Sathvika <suriyasa@infinite.com>
2026-05-20 13:37:38 +0530

    Assessment8 (Web Technologies) Completed

---
+++ Assessment 8(Web Technologies)/Assessment 8/Assessment 8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
